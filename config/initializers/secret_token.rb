85afb5dec8d586e70dc45347d8af94b1e978fef4
Thuong Ho <thuong.t.ho@gmail.com>
2014-07-16 22:11:45 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MovieRoom::Application.config.secret_key_base = 'c9098fb5f29fd68160717e6dc1582d497e31b6004e356c31ac7d1e5e61e611aed5f78c1035d9015e49e9c0fe04b4da0b30effb6395ce300cc3dc23e77ce204f0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MovieRoom::Application.config.session_store :cookie_store, key: '_movie_room_session'
